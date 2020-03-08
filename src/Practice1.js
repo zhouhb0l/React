1605d5fb81e5f148d87bf61fc1959d9e0f906a40
lengshi01 <lengshi01@users.noreply.github.com>
2020-03-08 21:51:15 +0800

    tic-tac-toe done

--- src/Practice1.js
+++ src/Practice1.js
@@ -44,6 +44,7 @@ class Board extends React.Component{
     constructor(props){
        super(props);
        this.state={squares:Array(9).fill(null),
+        xIsNext:true
     };
     }
     //always use super when defining constructor of a subclass. 
@@ -59,13 +60,22 @@ class Board extends React.Component{
 
      handleClick(i){
          const squares =this.state.squares.slice();
-         squares[i]='X';
-         this.setState({squares: squares});
+         if(calculateWinner(squares)||squares[i]){
+             return;
+         }
+         squares[i]=this.state.xIsNext ? 'X' : 'O';
+         this.setState({squares: squares,
+         xIsNext:!this.state.xIsNext});
      }
 //.clice is to create a copy of the squares
 
     render(){
-        const status="Next player: X"
+        const winner=calculateWinner(this.state.squares);
+        let status;
+        if(winner){
+            status="Winner: " + winner;
+        }else{
+        status='Next player: '+(this.state.xIsNext ? 'X' : 'O');}
 
         return(
             <div>
@@ -89,5 +99,27 @@ class Board extends React.Component{
         );
     }
 }
+
+function calculateWinner(squares){
+    const lines=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [1,4,7],
+        [2,5,8],
+        [0,3,6],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    for (let i = 0; i < lines.length; i++){
+        const [a,b,c]=lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a]===squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 
 export default Game
