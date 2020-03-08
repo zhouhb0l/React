85e3b2413251a177f34289c309f701fd0bc51e82
lengshi01 <lengshi01@users.noreply.github.com>
2020-03-08 18:19:15 +0800

    tic-tac-toe borad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Practice1 from './Practice1'
 function App() {
   return (
     <div className="App">
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React click here 
         </a>
       </header>
     </div>
---
+++ src/Practice1.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+
+
+class Game extends React.Component {
+    render(){
+        return(
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+             {/*<Practice1 />*/}   
+                <div className="game-info">
+                   <div>{/* status */}</div>
+                   <ol>{/*to do */}</ol>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+class Square extends React.Component{
+    render(){
+        return(
+            <button className="square">
+                {this.props.value}
+            </button>
+        )
+    }
+}
+
+class Board extends React.Component{
+    renderSquare(i){
+        return <Square value={i}/>;
+    }
+    render(){
+        const status="Next player: X"
+
+        return(
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+                </div>
+        );
+    }
+}
+
+export default Game
